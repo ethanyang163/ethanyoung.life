775a17bb3243b161952328d961b9bd2ce4a68d3a
Yuxiang Yang <yuxiang.yang@shanbay.com>
2020-10-30 11:38:01 +0800

    chore: change manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ethanyoung.life`,
+        short_name: `ethan`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/blog-icon.jpg`, // This path is relative to the root of the site.
       },
     },
     {
