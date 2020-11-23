da1f6531cf0cebee3a99166f73d7def2060115ab
Yuxiang Yang <yuxiang.yang@shanbay.com>
2020-11-23 21:36:47 +0800

    fix: fix google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,10 +112,12 @@ module.exports = {
             },
           },
           {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-              // The property ID; the tracking code won't be generated without it
-              trackingId: "G-816G9SMCXF",
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                "G-816G9SMCXF", // Google Analytics / GA
+              ],
             },
           },
         ],
